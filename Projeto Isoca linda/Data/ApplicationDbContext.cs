0e8ca9fb76e55a94fd53af255689c83aa39c1511
MaviSantos08 <maria.v.santos85@aluno.senai.br>
2025-10-29 07:50:28 -0300

    clientes e layout

--- Projeto Isoca linda/Data/ApplicationDbContext.cs
+++ Projeto Isoca linda/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Projeto_Isoca_linda.Models;
 
 namespace Projeto_Isoca_linda.Data
 {
@@ -8,6 +9,18 @@ namespace Projeto_Isoca_linda.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //Aqui, nós dizemos como irá se chamar a tabela 
+            //no banco de dados 
+            base.OnModelCreating(builder);
+            builder.Entity<Cliente>().ToTable("Clientes");
+            //Depois que voc~e cria vc pode add ao banco de dados 
+            // utilizando as migrations do Entity Framework
         }
     }
 }
---
+++ Projeto Isoca linda/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_Isoca_linda.Models
+{
+    public class Cliente
+    {
+        public Guid ClienteId { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        [StringLength(11, ErrorMessage = "O CPF deve ter 11 dígitos")]
+        public string CPF { get; set; }
+
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+        public string Email { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
